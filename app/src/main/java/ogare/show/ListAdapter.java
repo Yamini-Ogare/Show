b2ed94977789ef3c0638ada95a91250f63c964d1
Yamini-Ogare <yaminiogare@gmail.com>
2019-05-18 17:45:38 +0530

    intermidiate commit3

--- app/src/main/java/ogare/show/ListAdapter.java
+++ app/src/main/java/ogare/show/ListAdapter.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -18,6 +20,7 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -65,8 +68,6 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder >
             holder.linearLayout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
-
                     Intent intent = new Intent(context, DisplayActivity.class);
                     intent.putExtra("File", arrayList.get(position).getAbsoluteFile());
                     context.startActivity(intent);
@@ -78,16 +79,81 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder >
 
            //convert file to bitmap to set in image view
 
-          //  String filePath = arrayList.get(position).getPath();
-            /*Bitmap bitmap = BitmapFactory.decodeFile(filePath);*/
-
             Uri imageUri = Uri.fromFile(arrayList.get(position));
 
             Glide.with(context)
                     .load(imageUri)
                     .into(holder.imageview);
 
-         //   holder.imageview.setImageBitmap(bitmap);
+            holder.imageViewdel.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View view) {
+
+                    AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(context);
+
+                    LayoutInflater inflater = ((DisplayActivity)context).getLayoutInflater();
+                    View view1 = inflater.inflate(R.layout.alert_delete, null) ;
+                    dialogbuilder.setView(view1);
+
+                    final AlertDialog alertDialog = dialogbuilder.create();
+                    alertDialog.show();
+
+                    Button byes;
+                    Button bno;
+
+                    byes = view1.findViewById(R.id.delete_yes);
+                    bno = view1.findViewById(R.id.delete_no);
+
+                    byes.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            // delete file from storage
+
+                         /* boolean k=  arrayList.get(position).delete();
+                            if(arrayList.get(position).exists()){
+                                try {
+                                  boolean m=  arrayList.get(position).getCanonicalFile().delete();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                                if(arrayList.get(position).exists()){
+                                   boolean o = context.deleteFile(arrayList.get(position).getName());
+                                }
+                            }*/
+
+
+                     //   boolean t = arrayList.get(position).delete();
+                       //     arrayList.remove(position);
+
+                      String path = arrayList.get(position).getPath();
+
+                            arrayList.remove(position);
+                             try {
+                                boolean l = arrayList.get(position).getCanonicalFile().delete();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            notifyDataSetChanged();
+                            alertDialog.dismiss();
+
+                        }
+                    });
+
+                    bno.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                            alertDialog.dismiss();
+
+
+                        }
+                    });
+
+
+                }
+            });
 
         }
 
@@ -104,6 +170,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder >
         TextView textView;
         ImageView imageview;
         LinearLayout linearLayout;
+        ImageView imageViewdel ;
 
 
         public MyViewHolder(View itemView) {
@@ -112,6 +179,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder >
             textView = itemView.findViewById(R.id.name);
             imageview = itemView.findViewById(R.id.img);
             linearLayout = itemView.findViewById(R.id.detail);
+            imageViewdel = itemView.findViewById(R.id.delete_Img);
 
         }
 
